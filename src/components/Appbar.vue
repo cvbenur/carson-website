d9fa2bb94200e4b3bc843a135c97ab92941d721a
R-Nbt <rubenabet@gmail.com>
2020-09-09 15:09:47 +0200

    Start work on appbar

---
+++ src/components/Appbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <v-row>
+      <v-col cols="6">
+        <v-btn
+          icon
+          :elevation="4"
+        >
+          <v-img
+            alt="Carson Logo round"
+            contain
+            src="@/assets/carsonround.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Appbar extends Vue {
+}
+</script>
