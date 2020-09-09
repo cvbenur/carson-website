37601baa2b0eb308ebca2d1d4a01935860374008
R-Nbt <rubenabet@gmail.com>
2020-09-09 17:32:35 +0200

    Update appbar

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -3,23 +3,86 @@
     app
     color="primary"
     dark
+    :elevation="4"
+    :height="80"
   >
-    <v-row>
-      <v-col cols="6">
-        <v-btn
-          icon
-          :elevation="4"
-        >
+    <div class="mr-3 ml-2">
+      <v-btn
+        width="auto"
+        height="auto"
+        :to="'/'"
+        icon
+        color="transparent"
+        :elevation="4"
+      >
+        <v-avatar size="60">
           <v-img
             alt="Carson Logo round"
             contain
             src="@/assets/carsonround.png"
             transition="scale-transition"
-            width="40"
+            width="60"
+            aspect-ratio="1"
           />
+        </v-avatar>
+      </v-btn>
+    </div>
+
+    <v-avatar size="200">
+      <v-img
+        src="@/assets/ctext.png"
+        contain
+        alt="Carson Logo text"
+        transition="scale-transition"
+      />
+    </v-avatar>
+
+    <v-navigation-drawer
+      color="primary"
+      right
+      v-model="sidebar"
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+        >
+          <v-list-item-content>
+            {{ item.title }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar-items
+        class="hidden-xs-only"
+      >
+        <v-btn
+          text
+          depressed
+          class="text-none"
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+        >
+          {{ item.title }}
         </v-btn>
-      </v-col>
-    </v-row>
+      </v-toolbar-items>
+
+      <v-spacer></v-spacer>
+
+      <span v-if="!sidebar"
+        class="hidden-sm-and-up"
+      >
+        <v-btn
+          icon
+          @click="sidebar = true"
+        >
+          <v-icon>fas fa-bars</v-icon>
+        </v-btn>
+      </span>
   </v-app-bar>
 </template>
 
