098bffbdd58ac04971b147ab70e184c9e437c9db
R-Nbt <rubenabet@gmail.com>
2020-09-09 15:06:51 +0200

    Add dynamic page title

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,15 +4,23 @@ import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
+const title = 'Carson - ';
+
 const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: {
+      title: `${title}The Simple Discord StatBot`,
+    },
   },
   {
     path: '/about',
     name: 'About',
+    meta: {
+      title: `${title}About`,
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -24,6 +32,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
