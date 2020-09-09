3d471936e1bab1d1c0e0cdfbde771c62553ab05f
R-Nbt <rubenabet@gmail.com>
2020-09-09 21:51:27 +0200

    Update appbar

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -6,48 +6,79 @@
     :elevation="4"
     :height="80"
   >
-    <div class="mr-3 ml-2">
+    <div>
       <v-btn
-        width="auto"
-        height="auto"
         :to="'/'"
-        icon
         color="transparent"
-        :elevation="4"
+        depressed
+        text
       >
-        <v-avatar size="60">
+        <v-avatar size="60" width="60" :elevation="4">
           <v-img
             alt="Carson Logo round"
             contain
             src="@/assets/carsonround.png"
             transition="scale-transition"
-            width="60"
             aspect-ratio="1"
+            href="/"
+          />
+        </v-avatar>
+
+        <v-avatar width="175" height="60" tile class="ml-4">
+          <v-img
+            src="@/assets/ctext.png"
+            contain
+            alt="Carson Logo text"
+            transition="scale-transition"
           />
         </v-avatar>
       </v-btn>
     </div>
 
-    <v-avatar size="200">
-      <v-img
-        src="@/assets/ctext.png"
-        contain
-        alt="Carson Logo text"
-        transition="scale-transition"
-      />
-    </v-avatar>
+    <v-toolbar-items
+      class="hidden-xs-only"
+    >
+      <v-btn
+        text
+        depressed
+        class="white--text text-none font-weight-black mx-2"
+        style="font-size: 1rem"
+        v-for="item in items"
+        :key="item.title"
+        :href="item.to"
+      >
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
 
     <v-navigation-drawer
       color="primary"
       right
       v-model="sidebar"
       app
+      temporary
+      absolute
     >
       <v-list>
+        <v-list-item>
+          <v-spacer></v-spacer>
+          <span v-if="sidebar"
+            class="hidden-sm-and-up my-2"
+          >
+            <v-btn
+              icon
+              @click="sidebar = false"
+            >
+              <v-icon>fas fa-times</v-icon>
+            </v-btn>
+          </span>
+        </v-list-item>
         <v-list-item
+          class="white--text text-none font-weight-black mx-2"
+          style="font-size: 1.75rem"
           v-for="item in items"
           :key="item.title"
-          :to="item.to"
+          :href="item.to"
         >
           <v-list-item-content>
             {{ item.title }}
@@ -56,33 +87,18 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-toolbar-items
-        class="hidden-xs-only"
-      >
-        <v-btn
-          text
-          depressed
-          class="text-none"
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-        >
-          {{ item.title }}
-        </v-btn>
-      </v-toolbar-items>
+    <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
-
-      <span v-if="!sidebar"
-        class="hidden-sm-and-up"
+    <span v-if="!sidebar"
+      class="hidden-sm-and-up"
+    >
+      <v-btn
+        icon
+        @click="sidebar = true"
       >
-        <v-btn
-          icon
-          @click="sidebar = true"
-        >
-          <v-icon>fas fa-bars</v-icon>
-        </v-btn>
-      </span>
+        <v-icon>fas fa-bars</v-icon>
+      </v-btn>
+    </span>
   </v-app-bar>
 </template>
 
@@ -100,7 +116,11 @@ export default class Appbar extends Vue {
     },
     {
       title: 'Commands',
-      to: '',
+      to: '/commands',
+    },
+    {
+      title: 'About',
+      to: '/about',
     },
   ];
 }
