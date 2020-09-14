59872ced68032403556daed145e60885baf8dbe0
R-Nbt <rubenabet@gmail.com>
2020-09-14 12:09:56 +0200

    Add a Command model

---
+++ src/models/command.ts
@@ -0,0 +1,21 @@
+export default class Command {
+  name: string;
+  category: string;
+  flags: Array<string>;
+  usage: string;
+  description: string;
+
+  constructor(
+    name: string,
+    category: string,
+    usage: string,
+    description: string,
+    flags: Array<string> = [],
+  ) {
+    this.name = name;
+    this.category = category;
+    this.flags = flags;
+    this.usage = usage;
+    this.description = description;
+  }
+}
