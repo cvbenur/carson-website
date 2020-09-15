4d07f6de4eabfca483c319c2cdcf778cf6ae04b4
R-Nbt <rubenabet@gmail.com>
2020-09-15 19:58:05 +0200

    Update styling

--- src/components/CommandTile.vue
+++ src/components/CommandTile.vue
@@ -2,25 +2,25 @@
   <v-container fluid class="pa-0">
     <v-card
       color="#820F54"
-      :elevation="2"
+      :elevation="3"
       @click="onClick"
       :ripple="false"
     >
-      <v-card-title class="white--text">
+      <v-card-title class="white--text command">
         <v-icon color="white" class="mr-2 pr-2" size="12px">
           {{ !expand ? 'fas fa-chevron-right' : 'fas fa-chevron-down' }}
         </v-icon>
 
         <span>
           <strong>
-            {{ commandName }}
+            {{ '-' + commandName }}
           </strong>
-           <span v-if="flags.length > 0">
+           <span v-if="args.length > 0">
             <span
-              v-for="(flag, i) in flags.length"
+              v-for="(arg, i) in args.length"
               :key="i"
             >
-              [{{ flag }}]<span v-if="i !== flags.length-1">, </span>
+              [{{ arg }}]<span v-if="i !== args.length-1">, </span>
             </span>
           </span>
         </span>
@@ -28,11 +28,7 @@
 
       <v-expand-transition>
         <div v-show="expand">
-          <v-card-text class="white--text">
-            {{ usage }}
-          </v-card-text>
-
-          <v-card-text class="white--text">
+          <v-card-text class="white--text pt-0 ml-5 command-description">
             {{ description }}
           </v-card-text>
         </div>
@@ -60,7 +56,7 @@ export default class Command extends Vue {
   usage!: string;
 
   @Prop({ required: false, default: null })
-  flags!: Array<string>;
+  args!: Array<string>;
 
   @Prop({ required: true })
   description!: string;
@@ -79,3 +75,12 @@ export default class Command extends Vue {
   }
 }
 </script>
+
+<style scoped>
+.command {
+  font-size: 1.3rem;
+}
+.command-description {
+  font-size: 1rem;
+}
+</style>
