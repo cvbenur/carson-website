071af41e2b2b7ef6af305c2b27903b0efeee2f98
R-Nbt <rubenabet@gmail.com>
2020-09-15 19:24:53 +0200

    Implement conditional tile expansion

--- src/components/CommandTile.vue
+++ src/components/CommandTile.vue
@@ -3,34 +3,56 @@
     <v-card
       color="#820F54"
       :elevation="2"
+      @click="onClick"
+      :ripple="false"
     >
       <v-card-title class="white--text">
-        {{ commandName }}
+        <v-icon color="white" class="mr-2 pr-2" size="12px">
+          {{ !expand ? 'fas fa-chevron-right' : 'fas fa-chevron-down' }}
+        </v-icon>
+
+        <span>
+          <strong>
+            {{ commandName }}
+          </strong>
+           <span v-if="flags.length > 0">
+            <span
+              v-for="(flag, i) in flags.length"
+              :key="i"
+            >
+              [{{ flag }}]<span v-if="i !== flags.length-1">, </span>
+            </span>
+          </span>
+        </span>
       </v-card-title>
 
-      <v-card-text
-        v-if="flags"
-        class="white--text"
-      >
-        {{ flags }}
-      </v-card-text>
+      <v-expand-transition>
+        <div v-show="expand">
+          <v-card-text class="white--text">
+            {{ usage }}
+          </v-card-text>
 
-      <v-card-text class="white--text">
-        {{ usage }}
-      </v-card-text>
-
-      <v-card-text class="white--text">
-        {{ description }}
-      </v-card-text>
+          <v-card-text class="white--text">
+            {{ description }}
+          </v-card-text>
+        </div>
+      </v-expand-transition>
     </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import {
+  Vue, Component, Prop, Emit,
+} from 'vue-property-decorator';
 
 @Component({})
 export default class Command extends Vue {
+  public expand: boolean = false;
+
+  @Prop({ required: true })
+  id!: number;
+
   @Prop({ required: true })
   commandName!: string;
 
@@ -42,5 +64,18 @@ export default class Command extends Vue {
 
   @Prop({ required: true })
   description!: string;
+
+  onClick() {
+    this.expand = !this.expand;
+    this.submit();
+  }
+
+  @Emit('clicked')
+  submit() {
+    return {
+      id: this.id,
+      open: this.expand,
+    };
+  }
 }
 </script>
