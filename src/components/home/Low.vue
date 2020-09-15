cd5494bbe6e5cfe42a552607c5917ff7cd3ed776
R-Nbt <rubenabet@gmail.com>
2020-09-15 22:10:17 +0200

    Continue work on home view

---
+++ src/components/home/Low.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container class="mx-0">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-img
+        contain
+        src="@/assets/img/low.svg"
+        class="bg"
+      ></v-img>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Low extends Vue {
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,88 +3,70 @@
     <v-row
       align="center"
       justify="center"
-      class="mx-auto"
-    >
-      <v-spacer></v-spacer>
-      <v-col cols="12" sm="8" class="pa-0">
-        <v-img
-          contain
-          class="svg-bg"
-          src="@/assets/bg.svg"
-          max-height="500px"
-          aspect-ratio="1"
-        ></v-img>
-      </v-col>
-    </v-row>
-
-    <v-row
-      align="center"
-      justify="center"
       class="mx-auto go-lower"
     >
-      <v-col order-sm="2" cols="12" sm="6" class="text-center">
-        <div
-          class="white--text font-weight-bold text-sm-left"
-          style="font-size: 3rem"
+      <v-col cols="12" sm="10">
+        <v-row
+          align="center"
+          justify="space-around"
+          class="mx-auto"
         >
-          ROCK YOUR STATS WITH CARSON
-        </div>
+          <v-col order-sm="2" cols="12" sm="6" class="text-center">
+            <span
+              class="white--text"
+              style="font-size: 3rem; font-weight: 900;"
+            >
+              ROCK YOUR STATS WITH CARSON
+            </span>
 
-        <v-spacer class="ma-5"></v-spacer>
+            <v-spacer class="ma-5"></v-spacer>
 
-        <v-btn
-          class="font-weight-bold text-none mx-auto"
-          style="font-size: 1.75rem"
-          rounded
-          dark
-          color="secondary"
-          :href="'https://discord.com/api/oauth2/authorize?client_id=696334203490664460&permissions=67497024&scope=bot'"
-          :elevation="10"
-          x-large
-          ripple
-        >
-          Add to Discord
-        </v-btn>
-      </v-col>
+            <v-btn
+              class="font-weight-bold text-none mx-auto"
+              style="font-size: 1.75rem"
+              rounded
+              dark
+              color="secondary"
+              :href="'https://discord.com/api/oauth2/authorize?client_id=696334203490664460&permissions=67497024&scope=bot'"
+              :elevation="10"
+              x-large
+              ripple
+              hover
+            >
+              Add to Discord
+            </v-btn>
+          </v-col>
 
-      <v-col order-sm="1" cols="12" sm="6">
-        <v-img
-          contain
-          src="@/assets/figure.svg"
-          max-height="500"
-        ></v-img>
+          <v-col order-sm="1" cols="12" sm="6">
+            <v-img
+              contain
+              src="@/assets/img/figure.svg"
+              max-height="500"
+            ></v-img>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
 
-    <v-row
-      align="center"
-      justify="center"
-      class="mx-auto"
-    >
-      <v-img
-        contain
-        src="@/assets/low.svg"
-        class="svg-low"
-      ></v-img>
-    </v-row>
+    <Low/>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import Low from '@/components/home/Low.vue';
 
-@Component({})
+@Component({
+  components: {
+    Low,
+  },
+})
 export default class Home extends Vue {
 }
 </script>
 
-<style lang="css">
-.svg-low {
-}
-
-.svg-bg {
-}
-
+<style scoped>
 .go-lower {
+  margin-bottom: -110px;
 }
 </style>
